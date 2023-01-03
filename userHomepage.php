84800ebb9526daa3ed1ec18a86ab285f84c20634
aGamer106 <agamer106@yahoo.com>
2023-01-03 22:08:27 +0200

    added a userHomepage.php page to display information about Customer's account;

---
+++ userHomepage.php
@@ -0,0 +1,30 @@
+<?php
+require 'user-navbar.php';
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/navbar.css">
+    <link rel="stylesheet" href="style/footer.css">
+    <link rel="stylesheet" href="style/homepage.css">
+    <title>Portal</title>
+</head>
+<body>
+
+
+
+
+
+
+<?php
+    require 'footer.php';
+?>
+
+</body>
+</html>
