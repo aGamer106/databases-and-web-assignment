45037556bee33360bc7b4dcd583349d84bd86d70
aGamer106 <agamer106@yahoo.com>
2023-01-03 22:10:26 +0200

    implemented a user navbar - available only when a Customer is logged in;

---
+++ user-navbar.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<div id="navbar">
+    <nav class="navbar-div">
+        <div class="home">
+            <a href="index.php"><img src="util/minigym.png" alt="" id="logo"> </a>
+        </div>
+        <div class="register">
+            <a class="navbar-register-button slide_left" href="logout.php">Logout</a>
+        </div>
+        <div class="login">
+            <a class="navbar-login-button slide_left" href="userHomepage.php">My Profile</a>
+        </div>
+    </nav>
+</div>
+
