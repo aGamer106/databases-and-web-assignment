4defb1a9c61a9615eb8e077d30e31d13e92a777a
aGamer106 <agamer106@yahoo.com>
2023-01-03 22:07:00 +0200

    added staff navbar;

---
+++ staff-navbar.php
@@ -0,0 +1,13 @@
+<div id="navbar">
+    <nav class="navbar-div">
+        <div class="home">
+            <a href="index.php"><img src="util/minigym.png" alt="" id="logo"> </a>
+        </div>
+        <div class="login">
+            <a class="navbar-login-button slide_left" href="customers.php">Manage Customers</a>
+        </div>
+        <div class="register">
+            <a class="navbar-register-button slide_left" href="register.php">Register</a>
+        </div>
+    </nav>
+</div>
