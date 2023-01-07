d33b333488dbd495775062c1ce416503ae3261b8
aGamer106 <agamer106@yahoo.com>
2023-01-07 23:11:22 +0200

    fixed staffLogin bug;

--- staffLoginSQL.php
+++ staffLoginSQL.php
@@ -7,10 +7,9 @@ function checkStaff () {
     }
 
     $db = new SQLite3('C:\\xampp\\data\\miniGym.db');
-    $stmt = $db->prepare('SELECT id, pwd FROM Auth 
-                                WHERE id=:id AND pwd=:password');
+    $stmt = $db->prepare('SELECT * FROM Staff LEFT JOIN Auth S on S.id = Staff.id WHERE S.id = :id AND pwd = :pwd;');
     $stmt->bindParam(':id', $_POST['id'], SQLITE3_TEXT);
-    $stmt->bindParam(':password', $_POST['pwd'], SQLITE3_TEXT);
+    $stmt->bindParam(':pwd', $_POST['pwd'], SQLITE3_TEXT);
     $result = $stmt->execute();
     $rows_array = [];
 
