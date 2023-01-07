971645284fba1d5747befcbfa0b369dfd0ba30d5
aGamer106 <agamer106@yahoo.com>
2023-01-07 23:07:06 +0200

    modified the navbar for staff;

--- staff-navbar.php
+++ staff-navbar.php
@@ -3,11 +3,17 @@
         <div class="home">
             <a href="index.php"><img src="util/minigym.png" alt="" id="logo"> </a>
         </div>
+        <div class="register">
+            <a class="navbar-register-button slide_left" href="logout.php">Logout</a>
+        </div>
         <div class="login">
             <a class="navbar-login-button slide_left" href="customers.php">Manage Customers</a>
         </div>
-        <div class="register">
-            <a class="navbar-register-button slide_left" href="register.php">Register</a>
+        <div class="payments">
+            <a class="navbar-login-button slide_left" href="payments.php">Manage Payments</a>
+        </div>
+        <div class="home">
+            <a class="navbar-register-button slide_left" href="staffHomepage.php">Home</a>
         </div>
     </nav>
 </div>
