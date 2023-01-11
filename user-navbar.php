305ccb9a69dcc51724d3c1587e68735a4732997f
aGamer106 <agamer106@yahoo.com>
2023-01-11 22:25:20 +0200

    updated user navbar with one more button;

--- user-navbar.php
+++ user-navbar.php
@@ -6,8 +6,12 @@
         <div class="home">
             <a href="index.php"><img src="util/minigym.png" alt="" id="logo"> </a>
         </div>
+
         <div class="register">
             <a class="navbar-register-button slide_left" href="logout.php">Logout</a>
+        </div>
+        <div class="register">
+            <a class="navbar-register-button slide_left" href="loginResult.php">Portal</a>
         </div>
         <div class="login">
             <a class="navbar-login-button slide_left" href="userHomepage.php">My Profile</a>
